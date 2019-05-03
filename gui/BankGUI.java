0f9d78ad976aaf8ae7028f4f5687be6b81fd7a2b
melissa526 <45842997+Melissa526@users.noreply.github.com>
2019-05-03 10:43:33 +0900

    Update BankGUI.java

--- gui/BankGUI.java
+++ gui/BankGUI.java
@@ -1,37 +1,40 @@
 package com.gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 import com.biz.BankBiz;
 import com.dto.BankDto;
-import java.awt.Color;
-import java.awt.Font;
-import javax.swing.SwingConstants;
-import javax.swing.ImageIcon;
 
 public class BankGUI extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
-	//1.
-	JPanel panel_transfer_top, panel_transfer_mid, panel_transfer_bottom;
-	JButton btn_transfer, btn_goback;
-	JTextField txt_account, txt_money, txt_message;
-	JLabel label_account, label_money, label_message;
-	BankBiz biz = new BankBiz();
-	BankDto my = biz.selectOne("01012341234");
 	private JLabel banner_transfer;
-	CheckPW passwordChk;
+	//1.
+	private JPanel panel_transfer_top, panel_transfer_mid, panel_transfer_bottom;
+	private JButton btn_transfer, btn_goback;
+	private JTextField txt_account, txt_money, txt_message;
+	private JLabel label_account, label_money, label_message;
+	private BankBiz biz = new BankBiz();
+	private BankDto my = biz.selectOne("01085278040");
+	private CheckPW passwordChk;
 
 	/**
 	 * Launch the application.
@@ -53,39 +56,45 @@ public class BankGUI extends JFrame implements ActionListener {
 	 * Create the frame.
 	 */
 	public BankGUI() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 600);
+		Dimension frameSize = this.getSize(); // 프레임 사이즈
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // 모니터 사이즈
+		this.setLocation((screenSize.width - frameSize.width)/2, (screenSize.height - frameSize.height)/2); 
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		
 		//2. 
 		panel_transfer_top = new JPanel();
-		panel_transfer_top.setBounds(0, 0, 800, 96);
+		panel_transfer_top.setBounds(0, 0, 800, 100);
 		panel_transfer_mid = new JPanel();
 		panel_transfer_mid.setBounds(5, 154, 790, 257);
 		panel_transfer_bottom = new JPanel();
-		panel_transfer_bottom.setBounds(5, 450, 790, 122);
+		panel_transfer_bottom.setBounds(0, 430, 800, 122);
 		
 		label_message = new JLabel("받는분 통장메모");
 		label_message.setBounds(143, 173, 150, 40);
 		label_message.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		label_message.setHorizontalAlignment(SwingConstants.CENTER);
 		
+		
 		txt_message = new JTextField(40);
 		txt_message.setBounds(294, 169, 354, 50);
 		btn_transfer = new JButton("이   체");
 		btn_transfer.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
-		btn_transfer.setBounds(238, 6, 150, 50);
+		btn_transfer.setBounds(241, 6, 150, 50);
 		btn_transfer.setBackground(new Color(32, 178, 170));
 		btn_transfer.setForeground(new Color(0, 0, 0));
 		btn_goback = new JButton("되돌아 가기");
 		btn_goback.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
-		btn_goback.setBounds(417, 7, 150, 50);
+		btn_goback.setBounds(420, 7, 150, 50);
 		panel_transfer_bottom.add(btn_goback);
 		contentPane.setLayout(null);
 		
 		panel_transfer_mid.setLayout(null);
-		label_account = new JLabel("계좌번호");
+		label_account = new JLabel("받는 분 계좌번호");
 		label_account.setBounds(143, 47, 150, 40);
 		label_account.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		label_account.setHorizontalAlignment(SwingConstants.CENTER);
@@ -112,25 +121,20 @@ public class BankGUI extends JFrame implements ActionListener {
 		btn_transfer.addActionListener(this);
 		btn_goback.addActionListener(this);
 		contentPane.setLayout(null);
-		
-		
-		
+
 		//4. 
 		getContentPane().add(panel_transfer_top);
+		ImageIcon img = new ImageIcon("images/banner_transfer.png");
+		Image preImg = img.getImage();  //ImageIcon을 Image로 변환.
+		Image afterImg = preImg.getScaledInstance(800, 100, java.awt.Image.SCALE_SMOOTH);
+		ImageIcon afterIcon = new ImageIcon(afterImg); //Image로 ImageIcon 생성
 		panel_transfer_top.setLayout(new GridLayout(0, 1, 0, 0));
-		
 		banner_transfer = new JLabel("");
 		panel_transfer_top.add(banner_transfer);
 		banner_transfer.setVerticalAlignment(SwingConstants.TOP);
-		banner_transfer.setIcon(new ImageIcon("/images/banner_main.png"));
+		banner_transfer.setIcon(afterIcon);
 		getContentPane().add(panel_transfer_mid);
 		getContentPane().add(panel_transfer_bottom);
-		
-
-		
-	}
-	public void action() {
-		
 	}
 	
 	@Override
@@ -142,13 +146,10 @@ public class BankGUI extends JFrame implements ActionListener {
 			transferMoney(my, trans_account, trans_money, trans_message);
 		}else if(e.getSource() == btn_goback) {
 			JOptionPane.showConfirmDialog(null, "이 페이지를 나가시겠습니까?","되돌아 가기",JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
-			
 		}
-	
 	}
 	
 	public void transferMoney(BankDto sender, String receiverAccount, int sendMoney, String message) {
-		//passwordChk.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		String processRes = "";
 		//(1)계좌 유효성 검사
 		if(biz.accountCheck(receiverAccount)) {
@@ -156,21 +157,31 @@ public class BankGUI extends JFrame implements ActionListener {
 			//(2)잔액 검사
 			if(my.getT_balance() >= sendMoney) {
 				System.out.println("++++++ 거래잔액 OK! ++++++");
-				//(3)예금주 확인
-				if(JOptionPane.showConfirmDialog(null, "["+biz.selectOne(receiverAccount).getName()+"]님께 보내시는게 맞습니까?") == 0) {
-					passwordChk = new CheckPW(this,"비밀번호 확인",sender);
-					passwordChk.setVisible(true);
-					if(passwordChk.isPwChk()) {
-						//(4)내 계좌 출금
-						processRes += biz.updateBalance(sender, sender.getAccount(), -sendMoney,"이체출금");
-						//(5)상대 계좌 입금
-						processRes += biz.updateBalance(sender, receiverAccount, sendMoney, message);
-						if(processRes.equals("22")) {
-							JOptionPane.showMessageDialog(null, "성공적으로 이체되었습니다.", "이체 완료!", JOptionPane.INFORMATION_MESSAGE);
-						}else {
-							JOptionPane.showMessageDialog(null, "이체가 실패되었습니다.\n다시 시도해주세요.", "이체 실패", JOptionPane.INFORMATION_MESSAGE);
+				//(3-0) 본인계좌 여부확인
+				if(!sender.getAccount().equals(receiverAccount)) {
+					//(3)예금주 확인
+					if(JOptionPane.showConfirmDialog(null, "["+biz.selectOne(receiverAccount).getName()+"]님께 보내시는게 맞습니까?") == 0) {
+						//(4) 비밀번호 확인
+						passwordChk = new CheckPW(this,"비밀번호 확인",sender);
+						passwordChk.setVisible(true);
+						if(passwordChk.isPwChk()) {
+							System.out.println("++++++ 비밀번호 확인 OK! ++++++");
+							//(5)내 계좌 출금
+							processRes += biz.updateBalance(sender, sender.getAccount(), -sendMoney,"이체출금");
+							//(6)상대 계좌 입금
+							processRes += biz.updateBalance(sender, receiverAccount, sendMoney, message);
+							if(processRes.equals("22")) {
+								JOptionPane.showMessageDialog(null, "성공적으로 이체되었습니다.", "이체 완료!", JOptionPane.INFORMATION_MESSAGE);
+								System.out.println("++++++ 계좌이체 OK! ++++++");
+							}else {
+								JOptionPane.showMessageDialog(null, "이체가 실패되었습니다.\n다시 시도해주세요.", "이체 실패", JOptionPane.INFORMATION_MESSAGE);
+								System.err.println("++++++ 계좌이체 실패! ++++++");
+							}
 						}
 					}
+				}else {
+					System.err.println("++++++ 본인계좌 이체 Fail! ++++++");
+					JOptionPane.showMessageDialog(null, "본인 계좌로 이체하실 수 없습니다.", "Error", JOptionPane.ERROR_MESSAGE);
 				}
 			}else {
 				System.err.println("++++++ 거래잔액 Fail! ++++++");
@@ -180,30 +191,5 @@ public class BankGUI extends JFrame implements ActionListener {
 			System.err.println("++++++ 계좌 유효성 Fail! ++++++");
 			JOptionPane.showMessageDialog(null, "존재하지않는 계좌번호입니다.\n다시 확인해주세요", "Error", JOptionPane.ERROR_MESSAGE);
 		}
-		
 	}
 }
-
-
-/*
-	
-	class BankEvent extends WindowAdapter implements ActionListener{
-	
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			boolean processRes = false;
-			
-			
-			
-		}
-	}
-	
-	
-	
-	
-
-}
-
- 
- */
